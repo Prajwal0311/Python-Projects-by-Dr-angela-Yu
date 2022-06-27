78fc601cb7f682c0202ceeb75c567ddfd089524c
Prajwal0311 <108257883+Prajwal0311@users.noreply.github.com>
2022-06-27 20:59:14 +0530

    Create Treasure_Game.py

---
+++ Treasure_Game.py
@@ -0,0 +1,54 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1=input("You are at the cross road.Where do you want to go? Left or Right.")
+choice1_lower=choice1.lower()
+
+if choice1_lower=="left":
+  
+  choice2=input("Do you want to swim or wait till the boat arrives? Swim or Wait.")
+  choice2_lower=choice2.lower()
+
+  if choice2_lower=="wait":
+    choice3_lower=input("You are into a Tunnel. Which direction do you want to go? East West North or South.").lower()
+
+    if choice3_lower=="west":
+      print("You entered the city of Gold. Congratulations")
+    elif choice3_lower== "north":
+      print("You were run over by a train . Game over.")
+    elif choice3_lower=="south":
+      print("You entered a  Dry Desert with no habitation. Game over.")
+    elif choice3_lower=="east":
+      print("You entered a burning tunnel.Game over")
+  else:
+    print("You were eaten by hungry Crocodile. Game Over.")
+
+else:
+  print("You met an Accident and died. Game over")
+  
+
